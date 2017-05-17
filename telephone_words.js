ae7d92bc9b2de0baf2c51b70d77c32710c670e98
Neil Murphy <wnmurphy@gmail.com>
2017-05-17 11:52:30 -0700

    Modify telephone_words.js to accept string of any length.

--- telephone_words.js
+++ telephone_words.js
@@ -3,8 +3,8 @@
   Return all permutations, not only English words.
 */
 
-function telephoneWords (fourDigitString) {
-  fourDigitString = fourDigitString.toUpperCase().split('');
+function telephoneWords (digitString) {
+  digitString = digitString.toUpperCase().split('');
   var keymap = {
     1: null,
     2: ["a","b","c"],
@@ -22,18 +22,18 @@ function telephoneWords (fourDigitString) {
   // Recursively build all alphanumeric combinations.
   function buildCombos (combo, i) {
     // Add to storage and exit if we've completed one combination.
-    if (combo.length === 4) {
+    if (combo.length === digitString.length) {
       return combos.push(combo);
     }
-    // For each digit in fourDigitString, 
-    for (; i < fourDigitString.length; i++) {
+    // For each digit in digitString, 
+    for (; i < digitString.length; i++) {
       // If digit is either 0 or 1, add that to the string.
-      if (keymap[fourDigitString[i]] === null) {
-        buildCombos(combo + fourDigitString[i].toString(), i+1);
+      if (keymap[digitString[i]] === null) {
+        buildCombos(combo + digitString[i].toString(), i+1);
       } else {
         // Otherwise, start a recursive branch that replaces that digit for each of its alphanumerica options.
-        for (var j = 0; j < keymap[fourDigitString[i]].length; j++) {
-          buildCombos(combo + keymap[fourDigitString[i]][j], i+1);
+        for (var j = 0; j < keymap[digitString[i]].length; j++) {
+          buildCombos(combo + keymap[digitString[i]][j], i+1);
         } 
       }
     }
